449e499fa3458f2097b004e874b7b1eea6a4b420
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-08-13 16:22:20 -0700

    feat: Add functionality to the close button to close the menu and make the menu button appear again

--- src/scripts/button.js
+++ src/scripts/button.js
@@ -1,8 +1,19 @@
 const mb = document.getElementById('menuButton');
+const bd = document.getElementById('buttonDiv');
 const menu = document.getElementById('menuDiv');
+const close = document.getElementById('closeButton');
 
 mb.addEventListener('click', () => {
     menu.classList.remove('hidden')
     menu.classList.add('show')
+    bd.classList.remove('show')
+    bd.classList.add('hidden')
     console.log('test')
+})
+
+close.addEventListener('click', () => {
+    menu.classList.remove('show')
+    menu.classList.add('hidden')
+    bd.classList.remove('hidden')
+    bd.classList.add('show')
 })
